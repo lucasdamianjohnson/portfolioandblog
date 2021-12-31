4b52db72e88c5656b228e572e9b66a137a858f78
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2021-12-31 10:18:19 -0500

    'new'

---
+++ _next/static/TCMHI25E-4Me59-nAVsOO/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fportfolio","\u002Fblog","\u002Fportfolio\u002Fitems\u002F[id]","\u002Fblog\u002Fposts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
