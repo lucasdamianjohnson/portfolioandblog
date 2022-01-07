ff26fd059fba66a8a1df7f89f9992246e3b4b0a3
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2022-01-06 21:38:08 -0500

    'new'

--- image-jpg-to-png.sh
+++ image-jpg-to-png.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 `for file in $(find images -name '*.png'); do convert "$file"  "${file%.*}.jpg"; done`
-
+`for file in $(find images -name '*.PNG'); do convert "$file"  "${file%.*}.jpg"; done`
 
 
--- image-script.sh
+++ image-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #root images
-`for file in $(find images -name '*.jpg' not -name '*-thumb.jpg'); do convert "$file" -resize 50% "${file%.*}-thumb.jpg"; done`
-`for file in $(find images -name '*.png') not -name '*-thumb.jpg'); do convert "$file" -resize 50% "${file%.*}-thumb.png"; done`
+`for file in $(find images -name '*.jpg'    ! -name '*-thumb.jpg'); do convert "$file" -resize 50% "${file%.*}-thumb.jpg"; done`
+`for file in $(find images -name '*.png' ! -name '*-thumb.png'); do convert "$file" -resize 50% "${file%.*}-thumb.png"; done`
 `for file in $(find images -name '*.jpg'); do cwebp -q 80 "$file" -o "${file%.*}.webp"; done`
 `for file in $(find images -name '*.png'); do cwebp -q 80 "$file" -o "${file%.*}.webp"; done`
 
