1d08a71dcfa39b75d1f7d0aff40af178e8640c2e
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2024-04-30 15:19:00 -0400

    updated

--- out/image-png-to-jpg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-`for file in $(find images -name '*.png'); do convert "$file"  "${file%.*}.jpg"; done`
-`for file in $(find images -name '*.PNG'); do convert "$file"  "${file%.*}.jpg"; done`
-
-
