d50aff3a0b965b6eaff7aa3fa2684387a4eaccf5
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2024-10-30 18:10:36 -0400

    updated

--- out/_next/static/chunks/6914.94ee44378287a40c.js
+++
@@ -1,9 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6914],{6914:function(e,t,n){n.r(t),n.d(t,{postprocessVertexShaderWGSL:function(){return u}});var s=n(29427);let r="postprocessVertexShader",i=`attribute position: vec2<f32>;uniform scale: vec2<f32>;varying vUV: vec2<f32>;const madd=vec2(0.5,0.5);
-#define CUSTOM_VERTEX_DEFINITIONS
-@vertex
-fn main(input : VertexInputs)->FragmentInputs {
-#define CUSTOM_VERTEX_MAIN_BEGIN
-vertexOutputs.vUV=(vertexInputs.position*madd+madd)*uniforms.scale;vertexOutputs.position=vec4(vertexInputs.position,0.0,1.0);
-#define CUSTOM_VERTEX_MAIN_END
-}
-`;s.v.ShadersStoreWGSL[r]=i;let u={name:r,shader:i}}}]);
--- out/_next/static/chunks/8974.66c4a0853e200882.js
+++
@@ -1,4 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8974],{98974:function(e,t,r){r.r(t),r.d(t,{passPixelShaderWGSL:function(){return p}});var a=r(29427);let u="passPixelShader",n=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
-#define CUSTOM_FRAGMENT_DEFINITIONS
-@fragment
-fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=textureSample(textureSampler,textureSamplerSampler,input.vUV);}`;a.v.ShadersStoreWGSL[u]=n;let p={name:u,shader:n}}}]);
